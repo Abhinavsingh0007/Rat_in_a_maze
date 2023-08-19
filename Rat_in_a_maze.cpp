0723db82f230a8c0cd19b1aea9b28fb8eaf8ef64
Abhinavsingh0007 <100895071+Abhinavsingh0007@users.noreply.github.com>
2023-08-19 20:12:25 +0530

    Update Rat_in_a_maze.cpp

--- Rat_in_a_maze.cpp
+++ Rat_in_a_maze.cpp
@@ -1,9 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 void mazehelper(int maze[18][18], int n, int path[18][18] , int x, int y){
-    
     if(x == n-1 && y == n-1){
-		// we have reached some solution
+	// we have reached some solution
         // print the maze and return
         path[x][y] = 1;
         for(int i =0;i < n; i++){
@@ -15,7 +14,7 @@ void mazehelper(int maze[18][18], int n, int path[18][18] , int x, int y){
         cout << endl;
         return;
     }
-     if(x>=n || x<=-1 || y>=n || y<=-1 || maze[x][y]==0 || path[x][y]==1 ){
+    if(x>=n || x<=-1 || y>=n || y<=-1 || maze[x][y]==0 || path[x][y]==1 ){
         return;
     }
     
@@ -25,31 +24,30 @@ void mazehelper(int maze[18][18], int n, int path[18][18] , int x, int y){
     mazehelper(maze,n,path,x,y-1);
     mazehelper(maze,n,path,x,y+1);
     path[x][y] = 0;  
-// move at all possible positions and move to a smaller problem
-
+    // move at all possible positions and move to a smaller problem
 } 
 
-int main() {
-	int  n;
+int main() 
+{
+    int  n;
     cout<<"Enter the size of Maze : ";
-	cin >> n;
-	int maze[18][18];
+    cin >> n;
+    int maze[18][18];
     cout<<"Enter the path condition : 1 for path and 0 for wall"<<endl;
 	for(int i=0; i<n ; i++){
-		for(int  j=0 ; j<n ; j++){
-			cin >> maze[i][j];
-		}
-        
+	  for(int  j=0 ; j<n ; j++){
+		cin >> maze[i][j];
+	  }
 	}
     cout<<endl;
 
-	int path[18][18];
-	for(int i =0;i<n;i++){
-            for(int j = 0 ;j<n;j++){
+    int path[18][18];
+     for(int i =0;i<n;i++){
+        for(int j = 0 ;j<n;j++){
                 path[i][j] = 0;
             }
-        }
+     }
     cout<<"Possible paths :"<<endl;
-	mazehelper(maze,n,path,0,0);
-	return 0;
+    mazehelper(maze,n,path,0,0);
+    return 0;
 }
